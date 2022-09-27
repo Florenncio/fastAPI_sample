92756b5453e69a40ed14cec628a5e43b5de4d647
Fernando Florencio <nandoflorencio13@gmail.com>
2022-09-27 10:43:02 -0300

    #3.4 alterando o schemas do db

--- to_do/models.py
+++ to_do/models.py
@@ -1,43 +1,46 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Boolean
-from to_do.database import Base
+from sqlalchemy import Boolean, Column, DateTime, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
+
+from to_do.database import Base
 
 
 class User(Base):
-    
-    __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True, index=True)
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
     nome = Column(String)
     email = Column(String)
     password = Column(String)
-    
-    plans = relationship('Plan', back_populates='users')
+
+    plans = relationship("Plan")
+
 
 class Plan(Base):
 
-    __tablename__ = 'plans'
+    __tablename__ = "plans"
 
     id = Column(Integer, primary_key=True, index=True)
     titulo = Column(String)
+    descricao = Column(String)
     tag = Column(String)
-    
-    users_id = Column(Integer, ForeignKey('users.id'))
-    users = relationship('User', back_populates='plans')
-    
-    tasks = relationship('Task', back_populates='plans')
+    user_id = Column(Integer, ForeignKey("users.id"))
+
+    users = relationship("User", back_populates="plans")
+    tasks = relationship("Task")
+
 
 class Task(Base):
 
-    __tablename__ = 'tasks'
+    __tablename__ = "tasks"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True)
     titulo = Column(String)
     descricao = Column(String)
     prioridade = Column(Integer)
     data_inicio = Column(DateTime)
     data_final = Column(DateTime)
     concluido = Column(Boolean, default=False)
+    plan_id = Column(Integer, ForeignKey("plans.id"))
 
-    plans = relationship('Plan', back_populates='tasks')
-    plans_id = Column(Integer, ForeignKey('plans.id'))
+    plans = relationship("Plan", back_populates="tasks")
--- to_do/schemas.py
+++ to_do/schemas.py
@@ -1,100 +1,90 @@
-from enum import IntEnum, Enum
 from datetime import datetime
-from typing import List, Optional
-from pydantic import BaseModel
+from enum import Enum, IntEnum
+from typing import List
+
+from pydantic import BaseModel, EmailStr
 
 
-#Choises
-class PlanEnum(str, Enum):
-    pessoal = 'pessoal'
-    profissional = 'profissional'
-    estudos = 'estudo'
-    off_topic = 'off-topic'
+class Choice:
+    class PlanEnum(str, Enum):
+        pessoal = "pessoal"
+        profissional = "profissional"
+        estudos = "estudo"
+        off_topic = "off-topic"
 
-class TaskEnum(IntEnum):
-    baixa = 4
-    media = 3
-    alta = 2
-    urgente = 1
-
-
-#Tasks
-class TaskBase(BaseModel):
-    titulo:str
-    descricao:str
-    prioridade:TaskEnum
-    data_inicio:datetime
-    data_final:datetime
-    concluido:bool
-    plans_id:int
-
-class TaskCreate(TaskBase):
-    pass
-
-class TaskView(TaskBase):
-    titulo:str
-    descricao:str
-    prioridade:TaskEnum
-    data_inicio:datetime
-    data_final:datetime
-    concluido:bool
-    plans_id:int
-    
-    class Config():
-        orm_mode = True
-
-class TaskViewPlan(TaskBase):
-    titulo:str
-    descricao:str
-    prioridade:TaskEnum
-    data_inicio:datetime
-    data_final:datetime
-    concluido:bool
-    plans_id:int
-    
-    class Config():
-        orm_mode = True
+    class TaskEnum(IntEnum):
+        baixa = 4
+        media = 3
+        alta = 2
+        urgente = 1
 
 
-#Plans
-class PlanBase(BaseModel):
-    titulo:str
-    tag:PlanEnum
-    users_id:int
+class Task:
+    class Base(BaseModel):
+        titulo: str
+        descricao: str
+        prioridade: Choice.TaskEnum
+        data_inicio: datetime
+        data_final: datetime
+        concluido: bool = False
+        plan_id: int
 
-class PlanCreate(PlanBase):
-    pass
+        class Config:
+            orm_mode = True
 
-class PlanView(PlanBase):  
-    titulo:str
-    tag:PlanEnum
-    users_id:int
-    
-    class Config():
-        orm_mode = True
+    class Create(Base):
+        pass
 
-class PlanViewUser(PlanBase):  
-    titulo:str
-    tag:PlanEnum  
-    
-    class Config():
-        orm_mode = True
+    class View(Base):
+        titulo: str
+        descricao: str
+        prioridade: Choice.TaskEnum
+        data_inicio: datetime
+        data_final: datetime
+        concluido: bool
+        plan_id: int
 
-
-#Users
-class UserBase(BaseModel):
-    nome:str
-    email:str
-    password:str
-
-class UserCreate(UserBase):
-    pass
+        class Config:
+            orm_mode = True
 
 
-class UserView(BaseModel):
-    nome:str
-    email:str
-    plans:List[PlanViewUser] = []
+class Plan:
+    class Base(BaseModel):
+        titulo: str
+        descricao: str
+        tag: Choice.PlanEnum
+        user_id: int
 
-    class Config():
-        orm_mode = True
+        class Config:
+            orm_mode = True
+
+    class Create(Base):
+        pass
+
+    class View(Base):
+        titulo: str
+        descricao: str
+        tag: Choice.PlanEnum
+        user_id: int
+        tasks: List[Task.Base]
+
+        class Config:
+            orm_mode = True
+
+
+class User:
+    class Base(BaseModel):
+        nome: str
+        email: EmailStr
+        password: str
+
+    class Create(Base):
+        pass
+
+    class View(BaseModel):
+        nome: str
+        email: str
+        plans: List[Plan.Base]
+
+        class Config:
+            orm_mode = True
